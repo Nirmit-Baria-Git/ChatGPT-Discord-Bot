237341e277d9796d2d67aac133772d3db731a99e
Nirmit Baria <148620376+Nirmit-Baria-Git@users.noreply.github.com>
2023-12-19 19:22:38 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,56 @@
+import discord 
+import os
+import openai
+
+file = "1"
+match(file):
+  case "1":
+    file = "chat1.txt"
+  case "2":
+    file = "chat2.txt"
+  case "3": 
+    file = "chat3.txt"
+  case _:
+    print("Invalid choice.")
+    exit()
+    
+with open(file, "r") as f:
+  chat = f.read() 
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+token = os.getenv("SECRET_KEY")
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        global chat
+        try:
+          chat += f"{message.author}: {message.content}\n"
+          print(f'Message from {message.author}: {message.content}')
+          if self.user!= message.author:
+              if self.user in message.mentions:
+                response = openai.Completion.create(
+                  model="text-davinci-003",
+                  prompt = f"{chat}\nAgentGPT: ",
+                  temperature=1,
+                  max_tokens=256,
+                  top_p=1,
+                  frequency_penalty=0,
+                  presence_penalty=0
+                )
+                channel = message.channel
+                messageToSend = response.choices[0].text
+                await channel.send(messageToSend)    
+        except Exception as e:
+          print(e)
+          chat = ""
+            
+      
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(token)
